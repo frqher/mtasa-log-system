55211d453cf4608f8af07c9481f4a5397e98a9bc
Casti <20355378+frqher@users.noreply.github.com>
2022-11-22 00:08:26 +0300

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,21 @@
+function log(script, logs)
+	local dosya = nil
+	if fileOpen ( "loglar/"..script..".txt" ) then
+		dosya = fileOpen ( "loglar/"..script..".txt" )
+		eofdosya = fileGetSize ( dosya )
+		fileSetPos ( dosya, eofdosya )
+	else
+		dosya = fileCreate ( "loglar/"..script..".txt" )
+	end
+	if not dosya then 
+		restartResource(getThisResource())
+		return 
+	end
+	local time = getRealTime()
+	local curTime = "[".. time.year+1900 .."-".. time.month+1 .."-"..time.monthday.." "..time.hour..":"..time.minute.."]"
+	local str = curTime.." "..logs
+	str = str .. "\r\n"
+	eofdosya = fileWrite ( dosya, curTime.." "..logs .. "" .. "\n" )
+	fileSetPos ( dosya, eofdosya )
+	fileClose ( dosya )
+end
